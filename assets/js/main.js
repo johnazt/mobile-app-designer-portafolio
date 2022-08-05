9cc5317d10d816a3ae540565c26b7c3d427c60a2
johnazt <john.azt.dev@gmail.com>
2022-08-05 18:18:47 -0400

    Añadir modo desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
-function hola() {
-	console.log("hOls");
-}
+const navMenu = document.querySelector(".nav-list");
+const menuIcon = document.querySelector(".nav__menu-icon");
+const closeIcon = document.getElementById("closeIcon");
+
+menuIcon.addEventListener("click", function (e) {
+	menuIcon.style.display = "none";
+	navMenu.style.display = "flex";
+});
+
+closeIcon.addEventListener("click", function (evt) {
+	navMenu.style.display = "none";
+	menuIcon.style.display = "inline-block";
+});
+
+// window.onbeforeunload = function () {
+// 	window.scrollTo(0, 0);
+//   }
